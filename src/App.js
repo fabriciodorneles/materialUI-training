f40cb663e63eadc9eedcf70a678b5fc896045c11
Fabricio Dorneles <fabriciodorneles@gmail.com>
2020-10-13 08:14:13 -0300

    second test button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
+import { Button } from '@material-ui/core';
 
 function App() {
   return (
-    <div className="App">
-<h1>Hello World</h1>
-    </div>
-  );
+    <>
+      <Button variant="contained" color="primary"> Primary </Button>
+    </>)
 }
 
 export default App;
