e78e1da7995f38087dde0867b992395639f3d375
Fabricio Dorneles <fabriciodorneles@gmail.com>
2020-10-13 08:09:58 -0300

    first test button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import React from 'react';
+import { Button } from '@material-ui/core';
 
 function App() {
-  return (
-    <div className="App">
-<h1>Hello World</h1>
-    </div>
-  );
+  return <Button color="primary">Hello World</Button>;
 }
 
 export default App;
