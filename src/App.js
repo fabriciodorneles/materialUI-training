437f223a5a2bea6cb817b7a1def9eefdab8d246a
Fabricio Dorneles <fabriciodorneles@gmail.com>
2020-10-25 07:50:31 -0300

    makeStyles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
 import React from 'react';
-import { Button } from '@material-ui/core';
+import { Button, makeStyles, Typography, AppBar, Toolbar } from '@material-ui/core';
 
-function App() {
+
+
+
+const useStyles = makeStyles({
+  root: {
+    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    border: 0,
+    borderRadius: 3,
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    color: 'white',
+    height: 48,
+    padding: '0 30px',
+  },
+  titleStyle: {
+    fontStyle: 'oblique',
+    fontVariant: ''
+
+  }
+});
+
+function App()
+{
+  const classes = useStyles();
   return (
     <>
-    <Button color="primary">Hello World</Button>
-    <Button variant="contained" color="primary"> Primary </Button>
+      <div className="App" >
+        <Typography className={classes.titleStyle} variant="h1" color="secondary">Intro to Material UI</Typography>
+        <Button variant="outlined" color="secondary">Good Vibes!</Button>
+        <Button variant="contained" color="primary">Atalaskan</Button>
+        <Button variant="outlined" color="secondary">
+          <Typography variant="h2" color="initial"> helloko </Typography>
+        </Button>
+      </div>
     </>)
 }
 
---
+++ src/theme.js
@@ -0,0 +1,13 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+import purple from '@material-ui/core/colors/purple';
+import green from '@material-ui/core/colors/green';
+import { blue } from '@material-ui/core/colors';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: purple,
+    secondary: blue,
+  },
+});
+
+export default theme;
